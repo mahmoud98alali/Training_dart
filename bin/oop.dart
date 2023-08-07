7dac6965dd5a1c95a18d14f5bfc5a7fa387e1f70
mahmoud98alali <eng.mahmoud.alali@gmail.com>
2023-08-07 17:41:07 +0400

    override

--- bin/oop.dart
+++ bin/oop.dart
@@ -7,10 +7,30 @@ class Animal {
   int age;
   String name;
   Animal(this.age,this.name);
+
+  void eat (){
+    print("Eating");
+  }
 }
 
 class Cat extends Animal {
   Cat(super.age, super.name);
+// Cat(int a,String b) : super(a,b);
+
+@override
+void eat (){
+  print('Cat is eating');
+}
+}
+
+class Dog extends Animal {
+  Dog(super.age, super.name);
+// Cat(int a,String b) : super(a,b);
+
+}
+
+class Mouse extends Animal {
+  Mouse(super.age, super.name);
 // Cat(int a,String b) : super(a,b);
 
 }
